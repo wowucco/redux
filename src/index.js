1ccb0f88e469cde47f58182743ee957ef67a6e2c
wowucco <ckpet85@gmail.com>
2017-07-10 16:21:07 +0300

    add enhancer - logger, style

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import App from './containers/App'
+import './styles/app.css'
 import configureStore from './store/configureStore'
 
 const store = configureStore()
--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,50 @@
-var	path = require('path')
-var	webpack	= require('webpack')
+var path = require('path')
+var webpack = require('webpack')
+var autoprefixer = require('autoprefixer');
+var precss = require('precss');
+
 module.exports = {
-	devtool: 'cheap-module-eval-source-map',
-	entry: [
-		'webpack-hot-middleware/client',
-		'babel-polyfill',
-		'./src/index'
-	],
-	output:	{
-		path: path.join(__dirname, 'dist'),
-		filename: 'bundle.js',
-		publicPath:	'/static/'
-	},
-	plugins: [
-		new	webpack.optimize.OccurrenceOrderPlugin(),
-		new	webpack.HotModuleReplacementPlugin()
-	],
-	module:	{	//Обновлено
-		preLoaders:	[	//добавили	ESlint	в	preloaders
-			{
-				test: /\.js$/,
-				loaders: ['eslint'],
-				include: [
-					path.resolve(__dirname,	"src"),
-				],
-			}
-		],
-		loaders: [	//добавили	babel-loader
-			{
-				loaders: ['react-hot','babel-loader'],
-				include: [
-					path.resolve(__dirname,	"src"),
-				],
-				test: /\.js$/,
-				plugins: ['transform-runtime'],
-			}
-		]
-	}
+    devtool: 'cheap-module-eval-source-map',
+    entry: [
+        'webpack-hot-middleware/client',
+        'babel-polyfill',
+        './src/index'
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/static/'
+    },
+    plugins: [
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    module: {
+        preLoaders: [
+            {
+                test: /\.js$/,
+                loaders: ['eslint'],
+                include: [
+                    path.resolve(__dirname, "src"),
+                ],
+            }
+        ],
+        loaders: [
+            {
+                loaders: ['react-hot', 'babel-loader'],
+                include: [
+                    path.resolve(__dirname, "src"),
+                ],
+                test: /\.js$/,
+                plugins: ['transform-runtime'],
+            },
+            {
+                test:   /\.css$/,
+                loader: "style-loader!css-loader!postcss-loader"
+            }
+        ]
+    },
+    postcss: function () {
+        return [autoprefixer, precss];
+    }
 }
